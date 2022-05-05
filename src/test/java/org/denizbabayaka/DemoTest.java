f69e7d0a79badf78859d2c39d16712fe8472a38e
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-05-05 09:38:13 -0400

    new files added

--- src/test/java/org/denizbabayaka/DemoTest.java
+++ src/test/java/org/denizbabayaka/DemoTest.java
@@ -1,17 +1,23 @@
 package org.denizbabayaka;
 
 import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.denizbabayaka.TravelHomePage;
 import org.testng.annotations.Test;
 public class DemoTest {
     @Test
     public void flightTest() {
-        System.setProperty("webdriver.chrome.driver", "C://Users//deniz//OneDrive//Desktop//Selenium-Grid//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "C://Users//deniz//OneDrive//Desktop//Selenium-Grid//chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(chromeOptions);
         TravelHomePage travelHomePage = new TravelHomePage(driver);
         travelHomePage.goTo();
         travelHomePage.getFooterNav().selectFlight();
+
+
 
 
 
